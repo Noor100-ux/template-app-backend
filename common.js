6b816c6806eddd625c07ef6b76ffaaa928893c9b
Noor100-ux <112697081+Noor100-ux@users.noreply.github.com>
2025-04-03 18:13:35 +0530

    Update common.js

--- common.js
+++ common.js
@@ -2,31 +2,34 @@ const { default: mongoose } = require("mongoose");
 const { Template } = require("./model/user");
 
 async function createTenantDatabase(user) {
-    const email = user.email;
-    const username = email.split("@")[0];
-    const tenantDbName = `${username}_db`;
-    console.log(tenantDbName);
-    const tenantDbUri = `mongodb+srv://noormohamed100108:9ld4sXSrOZGxYPji@cluster0.yiqiz.mongodb.net/${tenantDbName}?retryWrites=true&w=majority&appName=Cluster0`;
+  const email = user.email;
+  const username = email.split("@")[0];
+  // Replace periods with underscores to avoid MongoDB database name restrictions
+  const sanitizedUsername = username.replace(/\./g, "_");
+  const tenantDbName = `${sanitizedUsername}_db`;
   
-    try {
-      console.log("entry");
-      const tenantConnection = mongoose.createConnection(tenantDbUri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+  console.log(`Creating database: ${tenantDbName}`);
   
-      tenantConnection.model("Template", Template.schema);
+  const tenantDbUri = `mongodb+srv://noormohamed100108:9ld4sXSrOZGxYPji@cluster0.yiqiz.mongodb.net/${tenantDbName}?retryWrites=true&w=majority&appName=Cluster0`;
+  
+  try {
+    console.log("Connecting to database...");
+    const tenantConnection = mongoose.createConnection(tenantDbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     
-      console.log(
-        `Tenant database created for user ${email} with db name ${tenantDbName}`
-      );
-  
-      return tenantDbUri
-    } catch (error) {
-      throw new Error(`Error creating tenant database: ${error.message}`);
-    }
+    tenantConnection.model("Template", Template.schema);
+    
+    console.log(`Tenant database created for user ${email} with db name ${tenantDbName}`);
+    
+    return tenantDbUri;
+  } catch (error) {
+    console.error(`Error creating tenant database:`, error);
+    throw new Error(`Error creating tenant database: ${error.message}`);
   }
+}
 
-  module.exports={
-    createTenantDatabase
-  }
+module.exports = {
+  createTenantDatabase
+};
